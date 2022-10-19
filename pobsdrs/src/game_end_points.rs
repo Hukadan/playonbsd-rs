ecdfcf43be498c804e4d5f376c04dd87631dddcc
Hukadan <me@hukadan.org>
2022-10-19 09:17:51 +0200

    Update search endpoint. The search can now be done on all relevant fields

--- pobsdrs/src/game_end_points.rs
+++ pobsdrs/src/game_end_points.rs
@@ -12,13 +12,20 @@ pub(crate) fn game_id(db: &State<DataBase>, id: usize) -> String {
     serde_json::to_string_pretty(&db.get_game_by_id(&id).expect("Should not fail")).unwrap()
 }
 
-#[get("/games?<name>")]
+#[get("/games?<name>&<engine>&<runtime>&<genre>&<tag>&<year>&<dev>&<publi>")]
 pub(crate) fn game_search_by_name(
     db: &State<DataBase>,
-    name: Option<String>,
+    name: Option<&str>,
+    engine: Option<&str>,
+    runtime: Option<&str>,
+    genre: Option<&str>,
+    tag: Option<&str>,
+    year: Option<&str>,
+    dev: Option<&str>,
+    publi: Option<&str>,
 ) -> String {
-    if let Some(name) = name {
-        return serde_json::to_string_pretty(&db.get_game_by_name_contains(&name)).unwrap()
-    }
-    serde_json::to_string_pretty(&db.get_all_games()).unwrap()
+    return serde_json::to_string_pretty(
+        &db.game_contains(name, engine, runtime, genre, tag, year, dev, publi),
+    )
+    .unwrap();
 }
