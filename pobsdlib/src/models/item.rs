563a56e6de7e1b39cbc77135a9dcf8a98797d774
Hukadan <me@hukadan.org>
2022-10-23 18:51:33 +0200

    Implement Display for Item

--- pobsdlib/src/models/item.rs
+++ pobsdlib/src/models/item.rs
@@ -1,5 +1,13 @@
+use std::fmt;
+
 #[derive(Serialize, Clone, Default, Debug, PartialEq)]
 pub struct Item {
     pub id: usize,
     pub name: String,
+}
+
+impl fmt::Display for Item {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+       write!(f, "Name\t{}", self.name)
+    }
 }
