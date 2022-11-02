8211b485728389b6629746dae480f66190264cb1
Hukadan <me@hukadan.org>
2022-11-02 12:30:23 +0100

    Update to the new api

--- pobsdrs/src/game_end_points.rs
+++ pobsdrs/src/game_end_points.rs
@@ -1,6 +1,7 @@
 use rocket::State;
 
 use pobsdlib::collections::DataBase;
+use pobsdlib::models::GameFilter;
 
 #[get("/games")]
 pub(crate) fn game_all(db: &State<DataBase>) -> String {
@@ -24,8 +25,33 @@ pub(crate) fn game_search_and(
     dev: Option<&str>,
     publi: Option<&str>,
 ) -> String {
+    let mut filter = GameFilter::new();
+    if let Some(name) = name {
+        filter.name_contains(name);
+    }
+    if let Some(engine) = engine {
+        filter.engine_contains(engine);
+    }
+    if let Some(runtime) = runtime {
+        filter.runtime_contains(runtime);
+    }
+    if let Some(genre) = genre {
+        filter.genre_contains(genre);
+    }
+    if let Some(tag) = tag {
+        filter.tag_contains(tag);
+    }
+    if let Some(year) = year {
+        filter.year_contains(year);
+    }
+    if let Some(dev) = dev {
+        filter.dev_contains(dev);
+    }
+    if let Some(publi) = publi {
+        filter.publi_contains(publi);
+    }
     return serde_json::to_string_pretty(
-        &db.game_contains_and(name, engine, runtime, genre, tag, year, dev, publi),
+        &db.game_contains_and(filter)
     )
     .unwrap();
 }
@@ -42,8 +68,33 @@ pub(crate) fn game_search_or(
     dev: Option<&str>,
     publi: Option<&str>,
 ) -> String {
+    let mut filter = GameFilter::new();
+    if let Some(name) = name {
+        filter.name_contains(name);
+    }
+    if let Some(engine) = engine {
+        filter.engine_contains(engine);
+    }
+    if let Some(runtime) = runtime {
+        filter.runtime_contains(runtime);
+    }
+    if let Some(genre) = genre {
+        filter.genre_contains(genre);
+    }
+    if let Some(tag) = tag {
+        filter.tag_contains(tag);
+    }
+    if let Some(year) = year {
+        filter.year_contains(year);
+    }
+    if let Some(dev) = dev {
+        filter.dev_contains(dev);
+    }
+    if let Some(publi) = publi {
+        filter.publi_contains(publi);
+    }
     return serde_json::to_string_pretty(
-        &db.game_contains_or(name, engine, runtime, genre, tag, year, dev, publi),
+        &db.game_contains_or(filter),
     )
     .unwrap();
 }
